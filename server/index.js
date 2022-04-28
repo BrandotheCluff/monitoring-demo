4f0b8f59d666b208b5569c3518f3c60731b7e77e
BrandotheCluff <brandon.cluff2@gmail.com>
2022-04-28 14:44:33 -0600

    names are monitored

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+app.use(express.json())
 
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
@@ -23,12 +24,19 @@ let students = []
 app.post('/api/student', (req, res)=>{
     let {name} = req.body
     name = name.trim()
+    const index = students.findIndex(studentName=> studentName === name)
 
-    students.push(name)
-
-    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
-
-    res.status(200).send(students)
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
 })
 
 
@@ -39,8 +47,6 @@ app.get('/', (req, res) => {
 
 
 
-
-app.use(express.json())
 
 const port = process.env.PORT || 4545
 
